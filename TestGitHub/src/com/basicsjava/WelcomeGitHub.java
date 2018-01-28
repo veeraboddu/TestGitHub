048ec0642b41ea3ed53cf944156a9dc8c7e0c4ea
veeraboddu <murthy.boddu@gmail.com>
2018-01-27 16:59:18 -0800

    first commit basic java

---
+++ TestGitHub/src/com/basicsjava/WelcomeGitHub.java
@@ -0,0 +1,12 @@
+package com.basicsjava;
+
+public class WelcomeGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to First class of Git HUB class");
+
+	}
+
+}
